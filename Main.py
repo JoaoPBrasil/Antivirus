147b15c4c6142402189637f542c39f61b9bb3d49
João Pedro Brasil <116403431+JoaoPBrasil@users.noreply.github.com>
2023-12-01 19:27:51 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,7 +2,6 @@ import tkinter as tk
 from tkinter import filedialog
 import os
 
-
 def verificar_arquivos():
     diretorio = filedialog.askdirectory(title="Selecione a pasta do arquivo")
     arquivos_encontrados = []
@@ -14,45 +13,39 @@ def verificar_arquivos():
 
     if arquivos_encontrados:
         resultado_label.config(text="Arquivos encontrados:")
+        lista_arquivos.delete(0, tk.END)  
         for arquivo in arquivos_encontrados:
             lista_arquivos.insert(tk.END, arquivo)
     else:
         resultado_label.config(text="Nenhum arquivo encontrado.")
 
-#botao para excluir
-
 def excluir_arquivo():
     index = lista_arquivos.curselection()
 
     if index:
- 
         caminho_arquivo = lista_arquivos.get(index)
-
         os.remove(caminho_arquivo)
-
         lista_arquivos.delete(index)
-
         resultado_label.config(text="Arquivo excluído com sucesso.")
     else:
         resultado_label.config(text="Nenhum arquivo selecionado.")
 
-# Criar a janela principal
 janela = tk.Tk()
 janela.title("Anti-virus")
 
+frame1 = tk.Frame(janela)
+frame1.pack(pady=10)
 
-selecionar_botao = tk.Button(janela, text="Selecionar pasta/arquivo", command=verificar_arquivos)
-selecionar_botao.pack(pady=10)
+selecionar_botao = tk.Button(frame1, text="Selecionar pasta/arquivo", command=verificar_arquivos)
+selecionar_botao.pack(side=tk.LEFT, padx=10)
 
+excluir_botao = tk.Button(frame1, text="Excluir arquivo", command=excluir_arquivo)
+excluir_botao.pack(side=tk.LEFT)
 
 resultado_label = tk.Label(janela, text="")
 resultado_label.pack()
 
-
 lista_arquivos = tk.Listbox(janela, selectmode=tk.SINGLE, width=100, height=15)
 lista_arquivos.pack(pady=10)
-
-excluir_botao = tk.Button(janela, text="Excluir arquivo", command=excluir_arquivo)
-excluir_botao.pack(pady=10)
 
 janela.mainloop()
